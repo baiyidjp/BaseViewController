808bd1ba2b6fc0ed5bfaa8b70945542996d8bde9
baiyidjp <271524878@qq.com>
2017-01-03 17:20:18 +0800

    Initial Commit

---
+++ BaseViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseViewController
+//
+//  Created by Keep丶Dream on 17/1/3.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
